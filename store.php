815fd91624d1cad60f055a404760f168b1b5acbc
KartikayTulsyan <122733780+KartikayTulsyan@users.noreply.github.com>
2023-03-25 17:16:42 +0530

    update

--- store.php
+++ store.php
@@ -1,14 +1,8 @@
 <?php
-$myfile = fopen("location.txt", "w");
-$txt = "position:" . $_GET["position"];
-fwrite($myfile, $txt);
-fclose($myfile);
-
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Headers: Content-Type");
-echo "Position stored successfully.";
-
+if (isset($_GET['position'])) {
+    // store the location data here
+    echo 'success';
+} else {
+    echo 'error';
+}
 ?>
-
